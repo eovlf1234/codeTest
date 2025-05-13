f92aa6110c10d608481e480742f3f96d9a295b8b
eovlf1234 <106236201+eovlf1234@users.noreply.github.com>
2025-05-13 17:11:49 +0900

    2025-05-13

--- codeTest/src/codeTest/Codeup_2369.java
+++ codeTest/src/codeTest/Codeup_2369.java
@@ -10,15 +10,26 @@ public class Codeup_2369 {
 		String inputs = sc.nextLine();
 		
 		String[] input = inputs.split(" ");
-		int min = 0;
-		for(String value : input) {
-			Integer.parseInt(value);
-		}
+		Integer[] div_num = {40, 50, 30};
 		
-		int min = Math.min(a/40, Math.min(b/50, c/30));
+		int min = Integer.parseInt(input[0]) / div_num[0];
+		
+		for(int i = 1; i < input.length; i++) {
+			int num = Integer.parseInt(input[i]) / div_num[i];
+			if(min > num) {
+				min = num;
+			}
+		}
 		
 		System.out.println(min);
 		
 	}
 
 }
+
+// 딱히 어려웠던 부분은 없었던것 같음
+// 1. if문에서 입력받은 숫자를 비교할 때 parseInt를 사용하고
+// 아래에서 min에 넣을 때 또 parseInt를 사용하는것과 
+// 2. num을 통해 for 안에서 parseInt를 사용하고 변수에 저장한 후
+// 그 num을 재사용 하는 것 중 어떤게 효율이 좋을지를 고민함.
+// chargpt를 통해 확인한 결과 2번을 사용하는 것이 효율이 좋은 것 같음
